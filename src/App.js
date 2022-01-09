f3bf780b9202f7b27b9fe5a6c3c59f41e3b6d447
lizjia <lisa.jia01@gmail.com>
2022-01-08 23:29:47 -0500

    profile created

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
         {/* <Route path ="" element = {}/> */}
         </Routes>
     </BrowserRouter>
+    <Profile/>
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,44 @@
-import photo from '../res/photo.png'
+import image from '../res/photo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FiSettings } from 'react-icons/fi';
+import { TiPencil } from 'react-icons/ti'
 
 
 const Profile = () => {
-    return (
-        <div class = "d-flex justify-content-around">
-        
+  return (
+
+    <div class="align-middle">
+      <div class="Container">
         <div class="row">
-            <div class="col-4">
-            <img src={photo} alt="User Photo" size = '96 x 6'></img>
-            <h2> Lisa</h2>
-            </div>
+          <div class="col-sm"></div>
+          <div class="col-sm">
+          <center>
+            <img src={image} alt="User Image"></img>
+            </center>
+            <h1> Lisa Jia</h1>
+            <h6> Student at Western University</h6>
+          </div>
+          <div class="col-sm"></div>
         </div>
+        <div class="row">
+          <div class="col-sm">
+          <center>
+            <button className='button-round'> <FiSettings size={75} /> </button>
+            </center>
+            <h5> Settings</h5>
+          </div>
+          <div class="col-sm">
+          <center>
+            <button className='button-round'> <TiPencil size={75} /> </button>
+            </center>
+            <h5> Edit Profile</h5>
+          </div>
+        </div>
+      </div>
     </div>
 
-    )
+
+  )
 }
 
 export default Profile 
